31db8dbdd948789874b8acd60d0a0e71b376da71
Jean Zombie <kappa_camus@tutanota.com>
2019-02-25 22:32:55 +0100

    remove more django settings

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -2,10 +2,9 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
 # wagtail specific
-from django.urls import re_path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
@@ -46,6 +45,13 @@ urlpatterns = [
 )
 
 if settings.DEBUG:
+    # Wagtail settings: Serve static and media files from development server
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
