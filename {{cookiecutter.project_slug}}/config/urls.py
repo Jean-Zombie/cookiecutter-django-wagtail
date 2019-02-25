c602837d722180df29e277b8615933d1f5c0efde
Jean Zombie <kappa_camus@tutanota.com>
2019-02-25 22:04:38 +0100

    remove django urlpatterns

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
-]
- + static(
+
+]  + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
 
