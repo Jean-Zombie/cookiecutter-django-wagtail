360c4cdfe889c3d6541e13062729295a5f3cfff0
Jean Zombie <kappa_camus@tutanota.com>
2020-01-15 14:46:09 +0100

    #

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/search/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/search/views.py
@@ -31,5 +31,5 @@ def search(request):
     return render(
         request,
         "search/search.html",
-        {"search_query": search_query, "search_results": search_results,},
+        {"search_query": search_query, "search_results": search_results},
     )
