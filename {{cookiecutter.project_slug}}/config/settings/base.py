11af16976b2b742aa30404d9832e05207dcbb6b5
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2020-08-12 17:59:21 +0200

    Remove Wagtails "SiteMiddleware" (deprecated)

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -169,7 +169,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
